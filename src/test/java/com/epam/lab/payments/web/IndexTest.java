9c01d2797fdeaffd638fcc80c5e5f8277c9dc4f6
Astel <astel.develop@gmail.com>
2017-11-12 20:04:13 +0300

    fix tests

--- src/test/java/com/epam/lab/payments/web/IndexTest.java
+++ src/test/java/com/epam/lab/payments/web/IndexTest.java
@@ -1,4 +1,4 @@
-package com.epam.lab.payments.controllers;
+package com.epam.lab.payments.web;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -6,16 +6,17 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@WithMockUser(roles="user")
 public class IndexTest {
 
     @Autowired
@@ -23,6 +24,7 @@ public class IndexTest {
 
     @Before
     public void setUp() throws Exception {
+
     }
 
     @Test
