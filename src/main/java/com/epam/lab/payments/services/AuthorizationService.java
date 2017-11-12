d0acf20e3e2885a116ce33fc3e5cfc4895bd1a9a
Astel <astel.develop@gmail.com>
2017-11-12 20:44:58 +0300

    fix tests

--- src/main/java/com/epam/lab/payments/services/AuthorizationService.java
+++ src/main/java/com/epam/lab/payments/services/AuthorizationService.java
@@ -3,11 +3,13 @@ package com.epam.lab.payments.services;
 import com.epam.lab.payments.dao.UserRepository;
 import com.epam.lab.payments.domain.UserEntity;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Log4j
 public class AuthorizationService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -15,9 +17,11 @@ public class AuthorizationService {
     public void save(UserEntity userEntity) {
         userEntity.setPassword(bCryptPasswordEncoder.encode(userEntity.getPassword()));
         userRepository.save(userEntity);
+        log.info("Save user " + userEntity);
     }
 
     public UserEntity findUserByEmail(String email) {
+        log.info("Save login " + email);
         return userRepository.findByEmail(email);
     }
 }
