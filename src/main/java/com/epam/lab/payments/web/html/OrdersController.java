2088a2ce7178fb1ffe1a86a93d36914b9a16735f
Sergei_Titov <titpost@yandex.ru>
2017-11-12 20:37:26 +0300

    thymeleaf added

--- src/main/java/com/epam/lab/payments/web/html/OrdersController.java
+++ src/main/java/com/epam/lab/payments/web/html/OrdersController.java
@@ -2,15 +2,17 @@ package com.epam.lab.payments.web.html;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OrdersController {
+
     @RequestMapping("/orders/{id}")
-    public String orders(@PathVariable("id") long id, Model model) {
+    public String orders(@PathVariable("id") String id, Model model) {
+
         model.addAttribute("accountId", id);
-        return "madeOrders";
+
+        return ("madeOrders");
     }
 }
