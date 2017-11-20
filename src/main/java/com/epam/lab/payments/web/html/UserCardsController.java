b2b7f47b1a7dc0e0b5bce4d1f5188e553550ced5
Titov Sergei <quosapv@yandex.ru>
2017-11-20 04:38:14 +0300

    adminPanel and user name added to report sheets

--- src/main/java/com/epam/lab/payments/web/html/UserCardsController.java
+++ src/main/java/com/epam/lab/payments/web/html/UserCardsController.java
@@ -1,6 +1,8 @@
 package com.epam.lab.payments.web.html;
 
 import com.epam.lab.payments.domain.UserEntity;
+import com.epam.lab.payments.dto.UserDTO;
+import com.epam.lab.payments.services.PaymentsService;
 import com.epam.lab.payments.services.UserDetailsServiceImpl;
 import com.epam.lab.payments.web.Roles;
 import lombok.RequiredArgsConstructor;
@@ -13,11 +15,13 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
 public class UserCardsController {
     private final UserDetailsServiceImpl userDetailsService;
+    private final PaymentsService paymentsService;
 
     @RequestMapping("/userCards/{id}")
     public ModelAndView userCards(@PathVariable("id") String id, HttpServletRequest request) {
@@ -26,11 +30,16 @@ public class UserCardsController {
         Principal principal = request.getUserPrincipal();
         modelAndView.addObject("principal", principal.getName());
 
-        modelAndView.addObject("userId", id);
-
+        // administrator's name
         UserEntity principalEntity = userDetailsService.loadUserEntityByUsername(principal.getName());
         modelAndView.addObject("firstName", principalEntity.getFirstName());
         modelAndView.addObject("lastName", principalEntity.getLastName());
+        modelAndView.addObject("userId", id);
+
+        // user's name
+        Optional<UserDTO> userDto = paymentsService.findOneUser(Integer.valueOf(id));
+        modelAndView.addObject("userFirstName", userDto.get().getFirstName());
+        modelAndView.addObject("userLastName", userDto.get().getLastName());
 
         boolean isAdmin = SecurityContextHolder
                 .getContext()
