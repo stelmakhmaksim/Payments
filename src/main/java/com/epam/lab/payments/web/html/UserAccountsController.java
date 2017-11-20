dace98fdcb40f2fadf32131f7c6fa76c0ac03169
Sergei_Titov <titpost@yandex.ru>
2017-11-20 15:42:37 +0300

    user Accounts title

--- src/main/java/com/epam/lab/payments/web/html/UserAccountsController.java
+++ src/main/java/com/epam/lab/payments/web/html/UserAccountsController.java
@@ -1,5 +1,13 @@
 package com.epam.lab.payments.web.html;
 
+import com.epam.lab.payments.domain.UserEntity;
+import com.epam.lab.payments.dto.UserDTO;
+import com.epam.lab.payments.services.PaymentsService;
+import com.epam.lab.payments.services.UserDetailsServiceImpl;
+import com.epam.lab.payments.web.Roles;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +15,13 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
+import java.util.Optional;
 
 @Controller
+@RequiredArgsConstructor
 public class UserAccountsController {
+    private final UserDetailsServiceImpl userDetailsService;
+    private final PaymentsService paymentsService;
 
     @RequestMapping("/userAccounts/{id}")
     public ModelAndView accounts(@PathVariable("id") String id, HttpServletRequest request) {
@@ -19,6 +31,24 @@ public class UserAccountsController {
         modelAndView.addObject("principal", principal.getName());
 
         modelAndView.addObject("userId", id);
+
+        // administrator's name
+        UserEntity principalEntity = userDetailsService.loadUserEntityByUsername(principal.getName());
+        modelAndView.addObject("firstName", principalEntity.getFirstName());
+        modelAndView.addObject("lastName", principalEntity.getLastName());
+        modelAndView.addObject("userId", id);
+
+        // user's name
+        Optional<UserDTO> userDto = paymentsService.findOneUser(Integer.valueOf(id));
+        modelAndView.addObject("userFirstName", userDto.get().getFirstName());
+        modelAndView.addObject("userLastName", userDto.get().getLastName());
+
+        boolean isAdmin = SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getAuthorities()
+                .contains(new SimpleGrantedAuthority(Roles.ADMIN.toString()));
+        modelAndView.addObject("isAdmin", isAdmin ? true : false);
 
         modelAndView.setViewName("reports/userAccounts");
         return modelAndView;
